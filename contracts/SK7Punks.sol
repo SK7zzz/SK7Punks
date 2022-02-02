53b1b978ab854b0bfa3b3568e63f5f62c2afb3b7
= <=>
2022-02-02 15:39:17 +0100

    NFTs DNA algorithms, bytes encoders, image and json generator. WTF I HAVE DONE

--- contracts/SK7Punks.sol
+++ contracts/SK7Punks.sol
@@ -6,9 +6,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 import "./Base64.sol";
+import "./SK7PunksDNA.sol";
 
-contract SK7Punks is ERC721, ERC721Enumerable, PaymentSplitter {
+contract SK7Punks is ERC721, ERC721Enumerable, PaymentSplitter, SK7PunksDNA {
     using Counters for Counters.Counter;
+    mapping(uint256 => uint256) public tokenDNA;
+
 
     Counters.Counter private _idCounter;
     uint public maxSupply;
@@ -22,11 +25,61 @@ contract SK7Punks is ERC721, ERC721Enumerable, PaymentSplitter {
         require(msg.value >= 50000000000000000, "You need 0.05 ETH to mint SK7Punks, A PAGAAAAAR");
         require(current < maxSupply, "No calipos left, only masibon");
 
+
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
         _safeMint(msg.sender, current);
         _idCounter.increment();
     }
 
- function tokenURI(uint256 tokenId)
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://avataaars.io/";
+    }
+
+    function _paramsURI(uint256 _dna) internal view returns (string memory) {
+        string memory params;
+
+        {
+            params = string(
+                abi.encodePacked(
+                    "accessoriesType=",
+                    getAccessoriesType(_dna),
+                    "&clotheColor=",
+                    getClotheColor(_dna),
+                    "&clotheType=",
+                    getClotheType(_dna),
+                    "&eyeType=",
+                    getEyeType(_dna),
+                    "&eyebrowType=",
+                    getEyeBrowType(_dna),
+                    "&facialHairColor=",
+                    getFacialHairColor(_dna),
+                    "&facialHairType=",
+                    getFacialHairType(_dna),
+                    "&hairColor=",
+                    getHairColor(_dna),
+                    "&hatColor=",
+                    getHatColor(_dna),
+                    "&graphicType=",
+                    getGraphicType(_dna),
+                    "&mouthType=",
+                    getMouthType(_dna),
+                    "&skinColor=",
+                    getSkinColor(_dna)
+                )
+            );
+        }
+
+        return string(abi.encodePacked(params, "&topType=", getTopType(_dna)));
+    }
+
+    function imageByDNA(uint256 _dna) public view returns (string memory) {
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+
+        return string(abi.encodePacked(baseURI, "?", paramsURI));
+    }
+
+    function tokenURI(uint256 tokenId)
         public
         view
         override
@@ -37,12 +90,16 @@ contract SK7Punks is ERC721, ERC721Enumerable, PaymentSplitter {
             "ERC721 Metadata: URI query for nonexistent token"
         );
 
+        uint256 dna = tokenDNA[tokenId];
+        string memory image = imageByDNA(dna);
+        
         string memory jsonURI = Base64.encode(
             abi.encodePacked(
                 '{ "name": "SK7Punk #',
                 tokenId,
                 '", "description": "SK7Punks are nfts made to learn how to interact with web3", "image": "',
-                "// TODO: Calculate image URL",
+                '"image": "',
+                image,
                 '"}'
             )
         );
